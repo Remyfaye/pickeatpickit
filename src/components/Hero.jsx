624ad9cc065cb354b68ab3f4a7aec1882a0d81c9
favour igbokwe <igbokwefc1@gmail.com>
2024-05-21 10:50:03 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="w-full">
+    <section className="w-full ">
       <Image
-        className="absolute w-full   h-[90vh] object-cover"
+        className="absolute w-full   h-[95%] object-cover"
         src="/bg.jpg"
         alt="img"
         width={500}
@@ -19,7 +19,15 @@ const Hero = () => {
         {/* header */}
         <header className=" flex items-center justify-between lg:px-10">
           {/* logo */}
-          <h1>PickEatPickIt</h1>
+          <Image
+            className=" object-cover"
+            src="/logo.png"
+            alt="img"
+            width={200}
+            height={200}
+            // layout="fill"
+            // objectFit="cover"
+          />
           <div className="flex items-center gap-2 justify-between">
             {/* <Image src="/logo.png" alt="img" width={200} height={200} /> */}
           </div>
@@ -48,10 +56,10 @@ const Hero = () => {
 
         {/* hero */}
         <div className="mt-[2rem] flex max-w-3xl mx-auto flex-col justify-center items-center">
-          <h1 className="text-3xl text-center lg:text-4xl lg:font-bold font-semibold mt-8">
+          <h1 className="header-text lg:header-text-lg lg:mt-10">
             your one-step destination for food delivery and courier services
           </h1>
-          <p className="my-5 text-center font-[inter] text-gray-200 font-extralight">
+          <p className="hero-small my-5 text-gray-300">
             satisfy your cravings and send packages with ease - all in one place
           </p>
 
@@ -62,27 +70,31 @@ const Hero = () => {
             </p>
             <button className="btn hidden lg:flex ">Search</button>
           </div>
-          <button className="btn mt-5 lg:hidden ">Search</button>
+          <button className="btn mt-8 lg:hidden ">Search</button>
         </div>
       </div>
 
       {/* form */}
-      <div className="flex mx-5 flex-col items-center shadow-xl bg-white mb-10 relative lg:mt-[-6rem] mt-[-13rem] max-w-2xl lg:mx-auto rounded-xl p-5">
-        <h1 className="font-bold text-2xl my-3">Send a package with ease</h1>
+      <div className="flex  mx-5 flex-col items-center shadow-xl bg-white mb-10 relative lg:mt-[-6rem] mt-[-15rem] max-w-2xl lg:mx-auto rounded-xl p-5">
+        <h1 className="hero-form-text font-bold">Send a package with ease</h1>
         <p className="mb-7 text-gray-400 text-sm">
           Fields for the courier service section
         </p>
-        <div className="grid grid-cols-2 gap-3">
-          <input placeholder="Pickup location" />
+        <div className="lg:grid grid-cols-2 gap-3">
+          <input className="border-[#FFD700]" placeholder="Pickup location" />
           <input placeholder="Drop off location" />
           <input placeholder="Type of item" />
           <input placeholder="weight " />
+          <textarea
+            className="mt-4 w-full lg:hidden "
+            placeholder="Additional details "
+          ></textarea>
         </div>
         <textarea
-          className="mt-4 w-full"
+          className="mt-4 hidden  lg:flex w-[75%] mx-auto"
           placeholder="Additional details "
         ></textarea>
-        <p className="text-gray-300 my-5">
+        <p className="text-gray-300 my-5 font-[200] text-[9px] lg:text-[15.27px]">
           Estimated cost and dilivery time dynamically displayed based on the
           user's input
         </p>
